2e7cc33a4afd2adadb16bc6cba15f242d660673d
gh-ita <hatimi.ghita@ensam-casa.ma>
2023-12-09 23:33:23 +0100

    replaced browserouter with hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import { HashRouter as Router,Routes, Route } from 'react-router-dom';
 import Music from './Components/Music.tsx';
 import Favoris from './Components/Favoris.js';
 
